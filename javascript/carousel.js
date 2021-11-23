66406ea981dc28da2f0b86f7c965a9533ed6a2b9
Kristof720 <94919979+Kristof720@users.noreply.github.com>
2021-11-23 17:03:44 +0100

    Add files via upload

---
+++ javascript/carousel.js
@@ -0,0 +1,7 @@
+$('.carousel').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true
+});
---
+++ javascript/menu.js
@@ -0,0 +1,3 @@
+$('.mobil__menu_icon').on("click", function(){
+    $('.header_navbar').toggle();
+});
